26e1ef77b8102159b4847418feff1a4b944adfa1
hoanganhtuanbk <anhtuanhnh@gmail.com>
2017-03-15 18:21:57 +0700

    a

--- client/index/js/components/main/home/our-clients.js
+++ client/index/js/components/main/home/our-clients.js
@@ -23,17 +23,19 @@ export class OurClients extends React.Component{
   }
   render(){
     const settings = {
-      dots: false,
-      speed: 3000,
+      dots: true,
+      infinite: true,
+      speed: 1500,
       autoplay: true,
-      lazyLoad: true,
-      slidesToShow: 6,
+      slidesToScroll: 1,
       arrows: false,
-      responsive: [ { breakpoint: 768, settings: { slidesToShow: 3 } }, { breakpoint: 1024, settings: { slidesToShow: 5 } }, { breakpoint: 1200, settings: 7 } ]
+      pauseOnHover: true,
+      slidesToShow: 6,
+      responsive: [ { breakpoint: 768, settings: { slidesToShow: 1 } }, { breakpoint: 1024, settings: { slidesToShow: 2 } }, { breakpoint: 1200, settings: 6 } ]
     };
     const childPartner = this.state.partners.map(function(result, index){
       return(
-        <div>
+        <div key={index}>
           <img src={result.url} alt={result.name} />
         </div>
       )
@@ -41,7 +43,7 @@ export class OurClients extends React.Component{
     return (
       <section className="section-5">
         <div className="container our-clients">
-          <div className="headline"><h2>Đối tác</h2></div>
+          <div className="headline"><h2>Đối tác1</h2></div>
           <Slider {...settings}>
             {childPartner}
           </Slider>
